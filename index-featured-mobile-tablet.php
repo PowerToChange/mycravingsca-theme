b4fe23ca14d039354ba3bc33e761c046f9afa89f
Jacques Robitaille <jacques.robitaille@gmail.com>
2013-03-22 15:17:27 -0400

    testing scrollsync without links

--- index-featured-mobile-tablet.php
+++ index-featured-mobile-tablet.php
@@ -10,11 +10,13 @@ if($front_page)
 ?>
 <div id="ts_wrapper" class="box mobile-tablet">
 	<div id="thumbslide">
+		<div id="dragger">
 <?php
 if($fas) foreach ($fas as $fa) {
 	echo get_the_post_thumbnail($fa['id'], 'mycravings_mobilethumb'); 
 }
 ?>
+		</div>
 	</div>
 </div>
 <?php
