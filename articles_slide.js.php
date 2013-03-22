832076d7b67fec4311e4e8d7ac0048ea54f45a4f
Jacques Robitaille <jacques.robitaille@gmail.com>
2013-03-22 16:04:31 -0400

    progressing to make touchSwipe work

--- articles_slide.js.php
+++ articles_slide.js.php
@@ -1,17 +1,36 @@
-<script src="<?php bloginfo('template_url') ?>/js/jquery.scrollsync.js"></script>
-<script>
-  $(function() {
-    
-    // Set header viewport to follow viewport scroll on x axis
-    $('#viewport, #header_viewport').
-        scrollsync({targetSelector: '#viewport', axis : 'x',mouseMoveHandler:function(){
-    console.log("H");
-    }});
-    
-    // Set drag scroll on first descendant of class dragger on both selected elements
-    $('#viewport, #ts_wrapper').
-        dragscrollable({dragSelector: '.dragger:first', preventDefault: true});
-     
-  });
+<script src="<?php bloginfo('template_url') ?>/js/jquery.touchSwipe.js"></script>
+<style>
+	.testbox
+{
+	margin-top:20px;
+	margin-bottom:20px;
+	max-width:768px;
+	height:300px;
+	
+	padding: 10px;
+	background-color: #EEE;
+	-webkit-border-radius: 6px;
+	-moz-border-radius: 6px;
+	border-radius: 6px;
+	
+	text-align:center;
+	font-weight: 300;
+	font-size: 20px;
+	line-height: 36px;
+}
 
+</style>
+<script id='code_1'>
+				$(function() {			
+					//Enable swiping...
+					$("#test").swipe( {
+						//Generic swipe handler for all directions
+						swipe:function(event, direction, distance, duration, fingerCount) {
+							$(this).text("You swiped " + direction );	
+						},
+						//Default is 75px, set to 0 for demo so any distance triggers swipe
+					   threshold:0
+					});
+				});
 </script>
+<div id="test" class="testbox">Swipe me</div>
