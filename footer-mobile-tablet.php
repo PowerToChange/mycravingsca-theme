c3743883074ff0f35d057c45ffdd965de9d72fca
Jacques Robitaille <jacques.robitaille@gmail.com>
2013-04-12 11:51:31 -0400

    added twitter script

--- footer-mobile-tablet.php
+++ footer-mobile-tablet.php
@@ -5,5 +5,35 @@
 			<span class="tk-nimbus-sans">OTHER POSTS &rarr;</span>
 		<?php } //if(is_front_page()) ?>
 		<div class="social-icons"><?php include('social-icons.php'); ?></div>
-	</div>
+					<script src="http://widgets.twimg.com/j/2/widget.js"></script> 
+					<script> 
+					new TWTR.Widget({
+					  version: 2,
+					  type: 'profile',
+					  rpp: 3,
+					  interval: 6000,
+					  width: 300,
+					  height: 250,
+					  theme: {
+					    shell: {
+					      background: '#878787',
+					      color: '#FFFFFF'
+					    },
+					    tweets: {
+					      background: '#878787',
+					      color: '#FFFFFF',
+					      links: '#A7C9CE'
+					    }
+					  },
+					  features: {
+					    scrollbar: false,
+					    loop: false,
+					    live: false,
+					    hashtags: true,
+					    timestamp: true,
+					    avatars: false,
+					    behavior: 'all'
+					  }
+					}).render().setUser('my_cravings').start();
+					</script> 	</div>
 </div>
