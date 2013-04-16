201b0987d8cb66c7c4d8316b579455bab1ea86e6
Jacques Robitaille <jacques.robitaille@gmail.com>
2013-04-16 14:29:22 -0400

    removed mail icon

--- social-icons.php
+++ social-icons.php
@@ -2,5 +2,5 @@
 	<a href="http://www.facebook.com/sharer.php?u=<?php the_permalink();?>&t=<?php the_title(); ?>" title="Share on Facebook." target="_blank">&#xF610;</a> <!-- facebook -->
 	<a href="http://twitter.com/home/?status=<?php the_title(); ?> - <?php the_permalink(); ?>" title="Tweet this!" target="_blank">&#xF611;</a> <!-- twitter -->
 	<a href="http://vimeo.com/p2cstudents" title="Visit our vimeo page" target="_blank">&#xF630;</a> <!-- video -->
-	&#x2709; <!-- email -->
+	<!-- &#x2709; <!-- email -->
 </span>
