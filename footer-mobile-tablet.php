3eef8ba01ca581dabfefdab999bcf4d9f12a6da6
Jacques Robitaille <jacques.robitaille@gmail.com>
2013-04-17 16:24:20 -0400

    removed OTHER POSTS for now

--- footer-mobile-tablet.php
+++ footer-mobile-tablet.php
@@ -1,7 +1,7 @@
 <div id="mobile-tablet-footer" class="mobile-tablet">
 	<div class="transparent-black-box"></div>
 	<div class="inside-content">
-		<?php if(is_front_page()) { ?>
+		<?php if(is_front_page() and false) { ?>
 			<span class="tk-nimbus-sans">OTHER POSTS &rarr;</span>
 		<?php } //if(is_front_page()) ?>
 		<div class="social-icons"><?php include('social-icons.php'); ?></div>
