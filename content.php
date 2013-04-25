1afb9df8c836cfa1c8e0c0247da355d1f7ec0b1d
Jacques Robitaille <jacques.robitaille@gmail.com>
2013-04-25 10:52:19 -0400

    changed the font of title if no image

--- content.php
+++ content.php
@@ -22,7 +22,7 @@
 				<?php the_post_thumbnail('full'); ?>
 			</div>
 			<?php else : ?>
-			<h1 class="entry-title">
+			<h1 class="entry-title tk-league-gothic">
 				<a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'twentytwelve' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a>
 			</h1>
 			<?php endif; // is_single() ?>
