84319932dc7f88ae9548469552a0bae1ea77af8b
Jacques Robitaille <jacques.robitaille@gmail.com>
2013-05-06 09:41:51 -0400

    added google analytics

--- footer.php
+++ footer.php
@@ -29,6 +29,7 @@
 <!-- optional .js for legacy browser support -->
 <script src="<?php bloginfo('template_url') ?>/css/social-circle/ss-social.js"></script>
 
+<!-- google analytics -->
 <script src="<?php bloginfo('template_url') ?>/js/fitvids.js"></script>
   <script>
     $(document).ready(function(){
@@ -36,5 +37,22 @@
       $("#page_wrapper").fitVids();
     });
   </script>
+  
+  <script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-2437988-36']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+<!-- /google analytics -->
+
+  
 </body>
 </html>
