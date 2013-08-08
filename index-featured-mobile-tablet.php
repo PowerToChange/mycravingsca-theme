280224e90ee1a0bc91e6443c72aa36f1d100f559
Jacques Robitaille <jacques.robitaille@gmail.com>
2013-08-08 10:16:54 -0400

    testing swiper

--- index-featured-mobile-tablet.php
+++ index-featured-mobile-tablet.php
@@ -17,6 +17,43 @@ foreach ($fas as $fa) {
 ?>
 	</ul>
 </div>
+  <div class="device">
+    <a class="arrow-left" href="#"></a> 
+    <a class="arrow-right" href="#"></a>
+    <div class="swiper-container">
+      <div class="swiper-wrapper">
+<?php
+foreach ($fas as $fa) {
+	?>
+        <div class="swiper-slide">
+          <div class="content-slide">
+	<?php mc_use_template_part_with_data('article-listed-with-excerpt.php', $fa);	?>
+          </div>
+        </div>
+<?php
+}
+?>
+      </div>
+    </div>
+    <div class="pagination"></div>
+  </div>
+  <script src="<?php bloginfo('template_url') ?>/js/idangerous.swiper-2.0.min.js"></script>
+  <script>
+  var mySwiper = new Swiper('.swiper-container',{
+    pagination: '.pagination',
+    loop:true,
+    grabCursor: true,
+    paginationClickable: true
+  })
+  $('.arrow-left').on('click', function(e){
+    e.preventDefault()
+    mySwiper.swipePrev()
+  })
+  $('.arrow-right').on('click', function(e){
+    e.preventDefault()
+    mySwiper.swipeNext()
+  })
+  </script>
 <?php
 		}
 ?>
