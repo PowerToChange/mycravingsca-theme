18294e884791603d3d4eef3320b5c79677837f8a
Jacques Robitaille <jacques.robitaille@gmail.com>
2013-08-09 22:15:20 -0400

    trying to fix about-us

--- page.php
+++ page.php
@@ -12,7 +12,7 @@
 get_header(); ?>
 
 	<div id="primary" class="site-content">
-		<div id="content" role="main">
+		<div id="content box" role="main">
 
 			<?php while ( have_posts() ) : the_post(); ?>
 				<?php get_template_part( 'content', 'page' ); ?>
