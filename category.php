6bcb5690dfc3c29c1b175575afbaf36e52fbb54e
Jacques Robitaille <jacques.robitaille@gmail.com>
2013-08-14 16:36:41 -0400

    fixed category pages

--- category.php
+++ category.php
@@ -17,25 +17,26 @@ get_header(); ?>
 		<div id="content" role="main">
 
 		<?php if ( have_posts() ) : ?>
-			<header class="archive-header">
-				<h1 class="archive-title"><?php printf( __( 'Category Archives: %s', 'twentytwelve' ), '<span>' . single_cat_title( '', false ) . '</span>' ); ?></h1>
+			<?php
+			the_post();
+			$thumbnail_full = mc_load('thumbnail_full');
+			rewind_posts();
+			?>
+			<div class="box">
+				<div class="front_article">
+					<div class="front_article_title_black_box"></div>
+					<div class="front_article_title tk-league-gothic"><?php echo single_cat_title( '', false ); ?></div>
+					<?php echo $thumbnail_full; ?>
+				</div>
 
 			<?php if ( category_description() ) : // Show an optional category description ?>
 				<div class="archive-meta"><?php echo category_description(); ?></div>
 			<?php endif; ?>
-			</header><!-- .archive-header -->
+
+			</div><!-- .box -->
 
 			<?php
-			/* Start the Loop */
-			while ( have_posts() ) : the_post();
-
-				/* Include the post format-specific template for the content. If you want to
-				 * this in a child theme then include a file called called content-___.php
-				 * (where ___ is the post format) and that will be used instead.
-				 */
-				get_template_part( 'content', get_post_format() );
-
-			endwhile;
+			require('article-list.php');
 
 			twentytwelve_content_nav( 'nav-below' );
 			?>
