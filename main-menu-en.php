52e20f0dbe742be30ca329462e2fe4891ab308fa
Jacques Robitaille <jacques.robitaille@gmail.com>
2013-08-14 20:26:05 -0400

    menu will show if user logged in

--- main-menu-en.php
+++ main-menu-en.php
@@ -3,16 +3,17 @@
             <ul id="nav" class="nav-menu">
                 <li><a href="<?php bloginfo('url'); ?>">Home</a></li>
                 <li><a href="<?php bloginfo('url'); ?>/about-us/">About Us</a></li>
-                <!--<li><a href="#">Cravings</a>
+<?php if( is_user_logged_in()) { ?>
+                <li><a href="#">Cravings</a>
                     <div class="subs">
                         <div>
                             <ul>
-                              <li><a href="<?php bloginfo('url'); ?>/power/">Power</a></li>
-                              <li><a href="<?php bloginfo('url'); ?>/acceptance/">Acceptance</a></li>
-                              <li><a href="<?php bloginfo('url'); ?>/security/">Security</a></li>
-                              <li><a href="<?php bloginfo('url'); ?>/pleasure/">Pleasure</a></li>
-                              <li><a href="<?php bloginfo('url'); ?>/purpose/">Purpose</a></li>
-                              <li><a href="<?php bloginfo('url'); ?>/meaning/">Meaning</a></li>
+                              <li><a href="<?php bloginfo('url'); ?>/category/power-2/">Power</a></li>
+                              <li><a href="<?php bloginfo('url'); ?>/category/acceptance-2/">Acceptance</a></li>
+                              <li><a href="<?php bloginfo('url'); ?>/category/security-2/">Security</a></li>
+                              <li><a href="<?php bloginfo('url'); ?>/category/pleasure/">Pleasure</a></li>
+                              <li><a href="<?php bloginfo('url'); ?>/category/purpose-2/">Purpose</a></li>
+                              <li><a href="<?php bloginfo('url'); ?>/category/meaning/">Meaning</a></li>
                         </div>
                     </div>
                 </li>
@@ -20,14 +21,15 @@
                     <div class="subs">
                         <div>
                             <ul>
-                              <li><a href="<?php bloginfo('url'); ?>/school/">School</a></li>
-                              <li><a href="<?php bloginfo('url'); ?>/culture/">Culture</a></li>
-                              <li><a href="<?php bloginfo('url'); ?>/life/">Life</a></li>
+                              <li><a href="<?php bloginfo('url'); ?>/category/school/">School</a></li>
+                              <li><a href="<?php bloginfo('url'); ?>/category/culture-2/">Culture</a></li>
+                              <li><a href="<?php bloginfo('url'); ?>/category/life-2/">Life</a></li>
                             </ul>
                         </div>
                     </div>
                 </li>
-                <li><a href="<?php bloginfo('url'); ?>/video/">Videos</a></li>-->
+                <li><a href="<?php bloginfo('url'); ?>/category/videos/">Videos</a></li>
+<?php } ?>
             </ul>
         </span>
     </div>
