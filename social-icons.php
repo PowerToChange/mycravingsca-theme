89a7f85624ca237759689c0e9491dec2c6eefb68
jacques <jacques.robitaille@gmail.com>
2014-02-12 11:20:38 -0500

    changed font icons for images

--- social-icons.php
+++ social-icons.php
@@ -1,7 +1,7 @@
 <h3 class="widget-title"><?php echo mc_t('Follow us on:'); ?></h3>
 <span class="ss-icon">
-	<a href="http://www.facebook.com/myCravings" title="Facebook" target="_blank">&#xF610;</a> <!-- facebook -->
-	<a href="http://twitter.com/my_cravings" title="Twitter" target="_blank">&#xF611;</a><!-- <a href="http://twitter.com/home/?status=<?php the_title(); ?> - <?php the_permalink(); ?>" title="Tweet this!" target="_blank">&#xF611;</a> <!-- twitter -->
-	<a href="http://vimeo.com/mycravings" title="Vimeo" target="_blank">&#xF630;</a> <!-- video -->
+	<a href="http://www.facebook.com/myCravings" title="Facebook" target="_blank"><img src="<?php bloginfo('template_url'); ?>/images/social/f.gif"></a> <!-- facebook &#xF610;-->
+	<a href="http://twitter.com/my_cravings" title="Twitter" target="_blank"><img src="<?php bloginfo('template_url'); ?>/images/social/t.gif"></a><!--&#xF611; <a href="http://twitter.com/home/?status=<?php the_title(); ?> - <?php the_permalink(); ?>" title="Tweet this!" target="_blank">&#xF611;</a> <!-- twitter -->
+	<a href="http://vimeo.com/mycravings" title="Vimeo" target="_blank"><img src="<?php bloginfo('template_url'); ?>/images/social/v.gif"></a> <!-- video &#xF630;-->
 	<!-- &#x2709; <!-- email -->
 </span>
